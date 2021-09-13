55c881969e2c6984718b3373e1420bdf69085dfc
Himanshu0894 <84079963+Himanshu0894@users.noreply.github.com>
2021-09-13 17:23:10 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,16 @@
+
+public class Max {
+
+	public static void main(String[] args) {
+           int x=60;
+           int y=45;
+           
+          if (x>y) {
+        	  System.out.println("X is Greater");
+          }
+          else {
+        	  System.out.println("Y is Greater");
+          }
+	}
+
+}
---
+++ Ternary.java
@@ -0,0 +1,15 @@
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		int x=58;
+		int y=42;
+		int max;
+		
+		max=(x>y)?x:y;
+		System.out.println(max);
+		
+
+	}
+
+}
